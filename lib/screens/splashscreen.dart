93b66bf41c6f5655cef925962876fc3c7e5362d9
Shanib c k <ckshanib8@gmail.com>
2023-09-01 10:22:07 +0530

    commands added in splashscreen.dart file

--- lib/screens/splashscreen.dart
+++ lib/screens/splashscreen.dart
@@ -1,7 +1,10 @@
+//This file is splashscreen.dart file
+
 import 'package:flutter/material.dart';
 import 'package:zerodays/screens/homescreen.dart';
 
 class SplashScreen extends StatefulWidget {
+  //This statfulwidget
   const SplashScreen({super.key});
 
   @override
@@ -12,7 +15,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     // TODO: implement initState
-    gotoLogin();
+    gotoLogin(); //function called
     super.initState();
   }
 
@@ -20,19 +23,24 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
+      //background color set
       body: Center(
         child: Image.asset(
           'asset/images/zerodays.jpg',
+          //splash image added
           width: 300,
+          //width set
         ),
       ),
     );
   }
 
   Future<void> gotoLogin() async {
+    //gotoLogin futuure function created
     await Future.delayed(
       const Duration(
         seconds: 3,
+        //duration time set
       ),
     );
     Navigator.of(context).pushReplacement(
