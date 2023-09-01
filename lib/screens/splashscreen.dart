ae93821e3aa1ca9e341e9038696b2863b5b57d0b
Shanib c k <ckshanib8@gmail.com>
2023-09-01 10:08:28 +0530

    statless widget converted into statfulwidget

--- lib/screens/splashscreen.dart
+++ lib/screens/splashscreen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
